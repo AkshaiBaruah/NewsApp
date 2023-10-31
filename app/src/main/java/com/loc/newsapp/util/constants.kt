42182f639575630f3574121bb08a4a125f4236d1
AkshaiBaruah <akshaib10@gmail.com>
2023-10-31 11:13:38 +0530

    Added paging and repository interface

--- app/src/main/java/com/loc/newsapp/util/constants.kt
+++ app/src/main/java/com/loc/newsapp/util/constants.kt
@@ -3,4 +3,5 @@ package com.loc.newsapp.util
 object constants {
     const val USER_SETTINGS : String = "UserSettings"
     const val APP_ENTRY = "AppEntry"
+    const val API_KEY = "e6e98fb3b4a74d76817602a28e125c70"
 }
